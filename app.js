d0498f3f2d48ba96e8912c9f91defdccf06f39d1
ManjunathRK1251 <rkmanjunath10000@gmail.com>
2022-08-04 13:29:47 +0530

    now circle follows the cursor

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ function setup()
 
 function draw()
 {
+    background("#fe934b55"); //last two digits represent the alpha channel values
+    fill("#ffffff");
+    noStroke();
     // draws a circle at location (30, 30) with diameter 20 pixels
-    circle(30,30,20);
+    circle(mouseX, mouseY,20);
+}
+
+function windowResized() 
+{
+    resizeCanvas(windowWidth, windowHeight);
 }
