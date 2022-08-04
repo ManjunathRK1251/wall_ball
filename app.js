49624edd4e592becbf275c404b4e8af3daffcd2d
ManjunathRK1251 <rkmanjunath10000@gmail.com>
2022-08-04 12:57:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// setup runs once
+function setup()
+{
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw()
+{
+    // draws a circle at location (30, 30) with diameter 20 pixels
+    circle(30,30,20);
+}
