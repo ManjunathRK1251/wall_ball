35f19e8efd6ee398297cfdcf0845aa88eaf78add
ManjunathRK1251 <rkmanjunath10000@gmail.com>
2022-08-04 13:40:07 +0530

    Add motion to the circle

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+let positionX, positionY;
+let speedX, speedY;
+
 // setup runs once
 function setup()
 {
     createCanvas(windowWidth, windowHeight);
+    positionX = 100;
+    positionY = 50;
+    speedX = 5; // 5 pixels per frame
+    speedY = 5;
 }
 
+// it is run everytime, for each frame in loop
 function draw()
 {
     background("#fe934b55"); //last two digits represent the alpha channel values
     fill("#ffffff");
     noStroke();
     // draws a circle at location (30, 30) with diameter 20 pixels
-    circle(mouseX, mouseY,20);
+    circle(positionX, positionY,20);
+
+    positionX += speedX; //this adds motion in X direction when each frame is rendered
+    positionY += speedY; //this adds motion in Y direction when each frame
 }
 
 function windowResized() 
